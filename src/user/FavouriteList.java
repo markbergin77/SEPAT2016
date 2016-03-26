58bb8d44633a62d8d0bf9344206bc4b591fc581d
lawrence <s3284184@student.rmit.edu.au>
2016-03-27 09:40:20 +1100

    FavouriteList won't take care of save/load, will happen externally

--- src/user/FavouriteList.java
+++ src/user/FavouriteList.java
@@ -1,34 +1,32 @@
 package user;
 
-import java.io.IOException;
 import java.util.Vector;
 
 import bomData.Location;
 
 public class FavouriteList extends Vector<Location> {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4022414816209237935L;
+
 	//Initialize vector of favourites
 	public static FavouriteList initialiseFavourites()
 	{
 		FavouriteList favourites = new FavouriteList();
 		return favourites;
 	}
-	
-	
-	//Saves file
-	public void saveFavouriteFile(FavouriteList favourites)
-	{
-		
-	}
+
 	//Opens file uses it to add to vector list
 	public void readFavouriteFile()
 	{
 		
 	}
+	
 	//Sorts the favourites vector on a number of views basis.
 	public void SortFavouriteList()
 	{
 		
 	}
-	
 }
