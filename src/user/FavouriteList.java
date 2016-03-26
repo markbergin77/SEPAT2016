8a96940884df2bc5fc979a9ec781b5e0cf4c1722
lawrence <s3284184@student.rmit.edu.au>
2016-03-27 08:15:36 +1100

    New package called "user" containing things like favourites

---
+++ src/user/FavouriteList.java
@@ -0,0 +1,34 @@
+package user;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import bomData.Location;
+
+public class FavouriteList extends Vector<Location> {
+	
+	//Initialize vector of favourites
+	public static FavouriteList initialiseFavourites()
+	{
+		FavouriteList favourites = new FavouriteList();
+		return favourites;
+	}
+	
+	
+	//Saves file
+	public void saveFavouriteFile(FavouriteList favourites)
+	{
+		
+	}
+	//Opens file uses it to add to vector list
+	public void readFavouriteFile()
+	{
+		
+	}
+	//Sorts the favourites vector on a number of views basis.
+	public void SortFavouriteList()
+	{
+		
+	}
+	
+}
