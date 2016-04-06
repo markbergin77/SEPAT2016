ab1b20b50965e1e6ec766c58f398fa6f6b6922ea
Mark Bergin <s3539565@student.rmit.edu.au>
2016-04-06 20:19:58 +1000

    Refactored function
    Moved FavouritesList around, felt that the implementation made more
    sense/was nicer in user class.

--- src/bomWeatherUser/FavouriteList.java
+++ src/bomWeatherUser/FavouriteList.java
@@ -15,26 +15,5 @@ public class FavouriteList extends Vector<Favourite> {
 		FavouriteList favourites = new FavouriteList();
 		return favourites;
 	}
-	
-	//Sorts the favourites  on a number of views basis.
-		public void SortFavourites(FavouriteList list)
-			{	
-				boolean notSorted = true;
-				while (notSorted)
-				{
-					notSorted = false;
-					for(int base = 0; base < list.size() - 1; base++)
-					{
-						if(list.get(base).timesViewed() < list.get(base+1).timesViewed())
-							{
-								Favourite temp = list.get(base);
-								list.set(base,list.get(base + 1));
-								list.set(base + 1, temp);
-								notSorted = true;
-							}	
-					}
-				}
-						
-			}
 
 }
