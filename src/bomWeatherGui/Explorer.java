0e8534d8bdf0769d4fe082316ec42d8e4e9b07e5
lawrence <lawrencem99@gmail.com>
2016-04-15 08:52:46 +1000

    About to branch

--- src/bomWeatherGui/Explorer.java
+++ src/bomWeatherGui/Explorer.java
@@ -1,18 +1,19 @@
 package bomWeatherGui;
 
+import java.awt.Dimension;
 import java.awt.Point;
 
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 
 // Currently has two tabs, all stations and favourite stations
-public class Explorer 
-{
-	StackPane rootStackPane;
+public class Explorer extends StackPane
+{	
 	
-	
-	public Explorer(Point maxSize)
+	public Explorer(Dimension bounds)
 	{
-		rootStackPane = new StackPane();
 		
 	}
+
+	
 }
