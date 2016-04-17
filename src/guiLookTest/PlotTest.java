1b09251269a7e1479957c576601ae42960751911
Mark Bergin <s3539565@student.rmit.edu.au>
2016-04-17 15:25:00 +1000

    Skeleton of what I'm working on
    Going to make some changes in the main branch...

---
+++ src/guiLookTest/PlotTest.java
@@ -0,0 +1,18 @@
+package guiLookTest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PlotTest extends Application{
+	public static void main(String args[])
+    {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage graph) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
