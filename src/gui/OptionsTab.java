263868fb73373dc133268a785b4dee1579655e2c
lawrence <lawrencem99@gmail.com>
2016-04-18 07:18:50 +1000

    No it didn't. Now it does

---
+++ src/gui/OptionsTab.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javafx.scene.control.Tab;
+
+/* Goes in the OptionsArea which is/has
+ * a TapPane.
+ */
+public class OptionsTab extends Tab
+{
+	
+	public OptionsTab(String name)
+	{
+		super();
+		setText(name);
+	}
+}
