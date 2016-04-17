57e70f245c62355e22d1eb081691b36f8ea58a9a
Mark Bergin <s3539565@student.rmit.edu.au>
2016-04-17 22:59:47 +1000

    Rough plot
    Worked with some JavaFX stuff.
    
    Just have to fill one more part of the requirement in regards to
    displaying temperatures, which I might do tonight or tomorrow.
    
    Will be discussing on what else we want to graph OTHER than temps on
    next team chat.

--- src/guiLookTest/PlotTest.java
+++ src/guiLookTest/PlotTest.java
@@ -1,18 +1,67 @@
 package guiLookTest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import data.Bom;
+import data.Station;
+import data.StationList;
+import data.Wthr72hr;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class PlotTest extends Application{
+	StationList allStations;
 	public static void main(String args[])
     {
         launch(args);
     }
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void start(Stage graph) throws Exception {
-		// TODO Auto-generated method stub
-		
+		//Grabbing stations
+		try {
+			allStations = Bom.getAllStations();
+	} catch (Exception e1) {
+		e1.printStackTrace();
+	}
+		//Just using Charlston as a test
+		Station charlston = allStations.get(0);	
+		graph.setTitle("Line Chart Sample");
+		//Category axis is needed for non ints, while number is needed for ints
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Every 30 mins");
+        yAxis.setLabel("Temperature in Degrees");
+        //Creates linechart based on string and number, can be changed to other things
+        final LineChart<String,Number> lineChart = new LineChart<String,Number>(xAxis,yAxis);              
+        lineChart.setTitle("TESTER");       
+        XYChart.Series temperatures = new XYChart.Series();
+        temperatures.setName(charlston.getName());
+        //Prints the weather for Charlston in the most recent time 
+        Wthr72hr tester = charlston.getWthrLast72hr();      
+        for(int i=tester.size()-1 ; i > 0; i--)
+        {
+        	/*Per requirements state
+        	 * b. The program should be able to graph the temperature history for each site
+			 * in the favourites list, including the max, min, 9am and 3pm temperatures 
+        	 */
+        	if(tester.get(i).getLocalDateTime().contains("09:00am") || tester.get(i).getLocalDateTime().contains("03:00pm"))
+        	{
+        		temperatures.getData().add(new XYChart.Data(tester.get(i).getLocalDateTime(),Float.parseFloat(tester.get(i).getAirTemp())));
+        	}
+        }
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(temperatures);
+       
+        graph.setScene(scene);
+        graph.show();
 	}
 	
 }
