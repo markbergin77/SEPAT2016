a6f5db271e7a98706eefe4d22b8fd22ff8b1a289
rmit-s3529812-radmilo-lega <s3529812@student.rmit.edu.au>
2016-04-20 19:16:36 +1000

    basic searching in StationButtonsPaneTest

--- src/guiLookTest/StationButtonsPaneTest.java
+++ src/guiLookTest/StationButtonsPaneTest.java
@@ -1,10 +1,19 @@
 package guiLookTest;
 
+import java.awt.Button;
+
 import data.Bom;
 import data.StationList;
+import gui.ExplorerButtonNotFav;
 import gui.ExplorerPaneAllStations;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class StationButtonsPaneTest extends Application
@@ -18,15 +27,41 @@ public class StationButtonsPaneTest extends Application
         StationList stations = Bom.getAllStations();
 		ExplorerPaneAllStations listPane = new ExplorerPaneAllStations();
 		listPane.createStationButtons(stations);
+		
+		// Basic Searching By Rad
+		ObservableList<Node> buttonList = listPane.getVBox().getChildren();
+		TextField searchBox = (TextField) listPane.getChildren().get(0);
+		searchBox.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+			{
+				for(Node button: buttonList) {
+					String label = ((ExplorerButtonNotFav) button).getText();
+					if (label.toLowerCase().contains(newValue.toLowerCase())) {
+						button.setManaged(true);
+					}
+					else {
+						button.setManaged(false);
+					}
+				}
+			}
+		});
+		
 		window.setScene(new Scene(listPane));
     	window.sizeToScene();
     	window.centerOnScreen();
     	window.show();
+    	
+    	TextField textField;
 	}
 	
 	public static void main(String args[])
     {
         launch(args);
     }
+	
+	public void handle(String text) {
+		
+	}
 
 }
