3d2be23ce32f3b785ab9e4a5abd50a223c4b682b
Mark Bergin <s3539565@student.rmit.edu.au>
2016-04-20 20:09:22 +1000

    Changed my special graph to some useful bonus text
    Looks shit now until styling.
    Is flexible to change,
    Placed the average function in Station.
    
    Also added some documentation stuff.

--- src/data/Station.java
+++ src/data/Station.java
@@ -278,7 +278,96 @@ public class Station implements Serializable
 		return samples;
 	}
 
+        public float[] findAverageTemps(Station station){
+        
+        WthrSamplesFine location = station.getWthrLast72hr();
 
+        //Need seperate list to associate each time of day with a specific "zone" or period
+        WthrSamplesFine earlyMorns = new WthrSamplesFine();
+        WthrSamplesFine middays = new WthrSamplesFine();
+        WthrSamplesFine nights = new WthrSamplesFine();
+        WthrSamplesFine lateNights = new WthrSamplesFine();
+        
+        //Goes through entire list of recorded location entries
+        for(int i=location.size()-1 ; i > 0; i--)
+        {
+        	int time24hours = 0;
+        	
+        	//Splits the given time date into it's raw form of xxxxam or pm
+        	String[] dateSplit = location.get(i).getLocalDateTime().split("/");
+        	String[] timeSplit = dateSplit[1].split(":");
+        	String timeconcat = timeSplit[0] + timeSplit[1];
+        	String finalTime;
+        	//Checks if it contains am or pm, if pm, we need to change it to 24 hour format
+        	if(timeconcat.contains("pm"))
+        	{
+        	String[] pmHourTime = timeconcat.split("pm");
+        	time24hours = Integer.parseInt(pmHourTime[0]);
+        	finalTime = Integer.toString(time24hours + 1200);
+        	}
+        	else{
+        	String[] amHourTime = timeconcat.split("am");
+        	time24hours = Integer.parseInt(amHourTime[0]);
+        	finalTime = Integer.toString(time24hours);
+        	}
+        	/* To deal with the late night period of 11pm to 4am
+        	 * we can't really check for a value in between 11pm and 4am 
+        	 * so we make 4am into a 24 hour next day kind of thing
+        	 * from 2300 - 0400 the next day we make 0400 = 2800 
+        	 */
+        	if(Integer.parseInt(finalTime) < 400)
+        	{
+        		int earlyHours = Integer.parseInt(finalTime);
+        		finalTime = Integer.toString(earlyHours + 2400);
+        	}
+        	
+        	if(500 <= Integer.parseInt(finalTime) & Integer.parseInt(finalTime) <= 1000)
+        	{        		
+        		earlyMorns.add(location.get(i));
+        		
+        	}
+        	//Weird situation checking inbetween periods of both am and pm (11am - 4pm), 12pm now turns to 2400 and we don't want to check 12
+        	//as that's 12 in the morning
+        	if(1100 <= Integer.parseInt(finalTime) & Integer.parseInt(finalTime) <= 1600 & Integer.parseInt(finalTime) != 1200 
+        	   || Integer.parseInt(finalTime) == 2400)
+        	{
+        		middays.add(location.get(i));	
+        	}
+        	if(1700 <= Integer.parseInt(finalTime) & Integer.parseInt(finalTime) <= 2200)
+        	{
+        		nights.add(location.get(i));
+        	
+        	}
+        	//once again, we don't want 2400 as that's 12 pm, we want am for this check
+        	if(2300 <= Integer.parseInt(finalTime) & Integer.parseInt(finalTime) <= 2800 & Integer.parseInt(finalTime) != 2400 
+             	   || Integer.parseInt(finalTime) == 1200)
+        	{
+        		lateNights.add(location.get(i));   		
+        	}
+        	
+        }   
+        
+        float[] sortedList = new float[4];
+        sortedList[0] = averageTemp(earlyMorns);
+        sortedList[1] = averageTemp(middays);
+        sortedList[2] = averageTemp(nights);
+        sortedList[3] = averageTemp(lateNights);
+        
+		return sortedList;
+	}
+	
+
+	float averageTemp(WthrSamplesFine samples)
+	{
+		float average = 0;
+		for(WthrSampleFine index : samples)
+		{
+			average += Float.parseFloat(index.getAirTemp());
+		}
+		average = average/samples.size();
+		return average;
+	}
+	
 	public String getName()
 	{
 		return name;
--- src/gui/OptionsPaneBase.java
+++ src/gui/OptionsPaneBase.java
@@ -1,8 +1,12 @@
 package gui;
 
+import java.text.DecimalFormat;
+
 import data.Station;
+import data.StationList;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.VBox;
 
@@ -12,17 +16,30 @@ import javafx.scene.layout.VBox;
 public class OptionsPaneBase extends VBox
 {
 	Station station;
-	GuiEventInterface eventHandler;
+	GuiEventInterface eventHandler;	
 	String button72HrText = "Open 72 Hour Temperature Plot";
 	Button open72HrPlotButton = new Button(button72HrText);
 	String buttonHisTempTxt = "Open Historical Temperature Plot";
 	Button OpenHisTempPlotButton= new Button(buttonHisTempTxt);
-	
 	public OptionsPaneBase(Station station, GuiEventInterface eventHandler)
 	{
 		super();
 		this.station = station;
 		this.eventHandler = eventHandler;
+		float[] averageTemps = new float[3];
+		DecimalFormat roundtotwo = new DecimalFormat("##.00");
+		averageTemps = station.findAverageTemps(station);
+		Label labelHead = new Label(" Average Temperatures over the last three days:");
+		Label labelEarlyMornings = new Label(" From 5am-10am: " + roundtotwo.format(averageTemps[0]) + " degress celcius");
+		Label labelMidDays = new Label(" From 11am-4pm: " + roundtotwo.format(averageTemps[1]) + " degress celcius");
+		Label labelNights = new Label(" From 5pm-10pm: " + roundtotwo.format(averageTemps[2]) + " degress celcius");
+		Label labelLateNights = new Label(" From 11pm-4am: " + roundtotwo.format(averageTemps[3]) + " degress celcius");
+		addOption(labelHead);
+		addOption(labelEarlyMornings);
+		addOption(labelMidDays);
+		addOption(labelNights);
+		addOption(labelLateNights);
+		
 		open72HrPlotButton.setOnMouseClicked(e -> eventHandler.onOpen72TempPlot(station));
 		OpenHisTempPlotButton.setOnMouseClicked(e -> eventHandler.onOpenHisTempPlot(station));
 		addOption(OpenHisTempPlotButton);
