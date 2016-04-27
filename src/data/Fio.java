de72fa7378ebc6892db66c1107358289e1bc8590
rmit-s3529812-radmilo-lega <s3529812@student.rmit.edu.au>
2016-04-27 16:01:20 +1000

    Forgot to track changes, really incompetent today...

---
+++ src/data/Fio.java
@@ -0,0 +1,61 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+public class Fio
+{
+	private static String baseUrl = "https://api.forecast.io/forecast/";
+	private static String apiKey = "04645e618b31cc361b70a8d5f7136f6f";
+
+	public static FioSamplesDaily getFioDaily(Station station)
+			throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException
+	{
+		String exclude = "[currently,minutely,hourly,alerts,flags]";
+		
+		// Need to get lat and lon from a wthrSample
+		WthrSamplesFine wthrSamplesFine = Bom.getWthrLast72hr(station);
+		String lon = wthrSamplesFine.get(0).getLon();
+		String lat = wthrSamplesFine.get(0).getLat();
+		
+		// Need to specify units=si for standard measurements (celcius)
+		String requestString = baseUrl + apiKey + "/" + lat + "," + lon + "?" + "units=si&exclude=" + exclude;
+		
+		
+		HttpURLConnection connection = (HttpURLConnection) new URL(requestString).openConnection();
+		connection.setRequestMethod("GET");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+		FioSamplesDaily samples = new FioSamplesDaily();
+		JsonArray rootArray = new JsonParser().parse(reader).getAsJsonObject().getAsJsonObject("daily").getAsJsonArray("data");
+
+		for (JsonElement element : rootArray)
+		{
+			JsonObject reading = element.getAsJsonObject();
+
+			String time = reading.get("time").getAsString();
+			String icon = reading.get("icon").getAsString();
+			String min = reading.get("temperatureMin").getAsString();
+			String max = reading.get("temperatureMax").getAsString();
+
+			samples.add(new FioSampleDaily(time, icon, min, max));
+		}
+		connection.disconnect();
+		return samples;
+	}
+}
---
+++ src/data/FioSamplesDaily.java
@@ -0,0 +1,8 @@
+package data;
+
+import java.util.Vector;
+
+public class FioSamplesDaily extends Vector<FioSampleDaily>
+{
+
+}
